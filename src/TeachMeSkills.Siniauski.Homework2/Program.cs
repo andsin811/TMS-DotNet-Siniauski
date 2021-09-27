89df24818c1934ef9d70bab42686bd7218e76c08
andsin811 <90961062+andsin811@users.noreply.github.com>
2021-09-27 19:51:17 +0300

    feat: add solution of homework 2

---
+++ src/TeachMeSkills.Siniauski.Homework2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace TeachMeSkills.Siniauski.Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string datePattern = @"^\d{4}-\d{2}-\d{2}";
+            Console.WriteLine("Enter 'exit' to exit.");
+            while (true)
+            {
+                Console.Write("Enter date (YYYY-MM-DD): ");
+                string dateStr = Console.ReadLine();
+                if (dateStr == "exit")
+                    break;
+                if (Regex.IsMatch(dateStr, datePattern) && DateTime.TryParse(dateStr, out DateTime date))
+                    Console.WriteLine($"Day is - {date.DayOfWeek}");
+                else
+                    Console.WriteLine("Error! Incorrect date!");
+            }
+        }
+    }
+}
